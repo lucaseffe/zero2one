342ac7e1d2a12f7315c8f13226412abdf7a174e6
Lucas Ferrari <lucasferrai@gmail.com>
2019-06-04 21:37:01 -0300

    teste

---
+++ back-end/src/main/java/br/com/zero2one/lucas/products/controller/ProductsController.java
@@ -0,0 +1,36 @@
+package br.com.zero2one.lucas.products.controller;
+
+import br.com.zero2one.lucas.products.model.Product;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/products")
+
+public class ProductsController {
+
+    private Map<String, Product> products = new HashMap<>();
+
+    public ProductsController(){
+        Product celular = new Product();
+        celular.setId("1A");
+        celular.setName("Moto X");
+        celular.setDescription("Novo Moto X2");
+        celular.setPrice(1999.99);
+        products.put("1A", celular);
+    }
+
+    @GetMapping
+    public List<Product> get(){
+        List<Product> products = new ArrayList<>();
+        return products;
+    }
+    @GetMapping("/{id}")
+    public Product getById(@PathVariable("id") String id){
+        return products.get(id);
+    }
+}
---
+++ back-end/src/main/java/br/com/zero2one/lucas/products/model/Product.java
@@ -0,0 +1,40 @@
+package br.com.zero2one.lucas.products.model;
+
+public class Product {
+    private String id;
+    private String name;
+    private String description;
+    private Double price;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+}
